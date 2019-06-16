f796330a5febef4f34416f3fb4df388f4c682f57
Bertrik Sikken <bertrik@sikken.nl>
2019-06-16 23:30:10 +0200

    Add custom parameter for luftdaten id.

--- stofananas.ino
+++ stofananas.ino
@@ -1,12 +1,15 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <EEPROM.h>
 
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
-#include "ESP8266HTTPClient.h"
+#include <ESP8266HTTPClient.h>
 #include <FastLED.h>
-#include "ArduinoJson.h"
+#include <ArduinoJson.h>
 
 #include "print.h"
 #include "cmdproc.h"
@@ -16,30 +19,63 @@
 
 #define DATA_PIN    D4
 
+#define SAVEDATA_MAGIC  0xCAFEBABE
+
+typedef struct {
+    char luftdatenid[16];
+    uint32_t magic;
+} savedata_t;
+
+static savedata_t savedata;
 static char esp_id[16];
+
 static WiFiManager wifiManager;
+static WiFiManagerParameter luftdatenIdParam("luftdatenid", "Luftdaten ID", "", sizeof(savedata_t));
 static WiFiClient wifiClient;
+
 static CRGB led;
 static char line[120];
+
+static void wifiManagerCallback(void)
+{
+    strcpy(savedata.luftdatenid, luftdatenIdParam.getValue());
+    savedata.magic = SAVEDATA_MAGIC;
+
+    print("Saving data to EEPROM: luftdatenid='%s'\n", savedata.luftdatenid);
+    EEPROM.put(0, savedata);
+    EEPROM.commit();
+}
+
 
 void setup(void)
 {
     Serial.begin(115200);
-    Serial.print("\nESP-STOFANANAS\n");
+    print("\nESP-STOFANANAS\n");
     EditInit(line, sizeof(line));
+
+    // init config
+    EEPROM.begin(sizeof(savedata));
+    EEPROM.get(0, savedata);
 
     // get ESP id
     sprintf(esp_id, "%08X", ESP.getChipId());
-    Serial.print("ESP ID: ");
-    Serial.println(esp_id);
-
-    // connect to wifi
-    Serial.println("Starting WIFI manager ...");
-    wifiManager.setConfigPortalTimeout(120);
-    wifiManager.autoConnect("ESP-STOFANANAS");
+    print("ESP ID: %s", esp_id);
 
     // config led
     FastLED.addLeds < PL9823, DATA_PIN > (&led, 1);
+    led = CRGB::Black;
+    FastLED.show();
+
+    // connect to wifi
+    print("Starting WIFI manager ...\n");
+    wifiManager.addParameter(&luftdatenIdParam);
+    wifiManager.setSaveConfigCallback(wifiManagerCallback);
+    wifiManager.setConfigPortalTimeout(120);
+    if (savedata.magic == SAVEDATA_MAGIC) {
+        wifiManager.autoConnect("ESP-STOFANANAS");
+    } else {
+        wifiManager.startConfigPortal("ESP-STOFANANAS");
+    }
 }
 
 static CRGB table[8] = {
@@ -72,10 +108,6 @@ static bool decode_json(String json, const char *item, float *value)
         for (JsonObject sensordatavalue:sensordatavalues) {
             const char *value_type = sensordatavalue["value_type"];
             const char *value = sensordatavalue["value"];
-            Serial.print("value_type: ");
-            Serial.println(String(value_type));
-            Serial.print("value: ");
-            Serial.println(String(value));
             if (strcmp(item, value_type) == 0) {
                 meas_num++;
                 meas_sum += strtof(value, NULL);
@@ -93,37 +125,53 @@ static bool decode_json(String json, const char *item, float *value)
 
 static int do_get(int argc, char *argv[])
 {
+    char url[64];
     WiFiClient wifiClient;
     HTTPClient httpClient;
 
     // perform the GET
     print("HTTP begin ...");
-    String url = "http://api.luftdaten.info/v1/sensor/12246/";
+    snprintf(url, sizeof(url), "http://api.luftdaten.info/v1/sensor/%s/", savedata.luftdatenid);
+    print("Executing GET to %s...", url);
     httpClient.begin(wifiClient, url);
-    print("GET ...");
     int res = httpClient.sendRequest("GET");
     print("code %d\n", res);
     String json = "";
     if (res == HTTP_CODE_OK) {
-        print("Response:");
         json = httpClient.getString();
     }
     httpClient.end();
 
-    Serial.print("JSON: ");
+    print("JSON: ");
     Serial.println(json);
 
     float pm = 0.0;
     if (decode_json(json, "P1", &pm)) {
-        Serial.printf("Particulate matter: %f\n", pm);
+        print("PM average: %f\n", pm);
     }
+
+    return 0;
+}
+
+static int do_config(int argc, char *argv[])
+{
+    if ((argc > 1) && (strcmp(argv[1], "clear") == 0)) {
+        print("Clearing config\n");
+        savedata.magic = 0;
+        EEPROM.put(0, savedata);
+        EEPROM.commit();
+    }
+    
+    print("config.luftdatenid = %s\n", savedata.luftdatenid);
+    print("config.magic       = %08X\n", savedata.magic);
 
     return 0;
 }
 
 const cmd_t commands[] = {
     { "help", do_help, "Show help" },
-    { "get", do_get, "Do a get" },
+    { "get", do_get, "Do HTTP GET" },
+    { "config", do_config, "Show/clear config" },
     { NULL, NULL, NULL }
 };
 
