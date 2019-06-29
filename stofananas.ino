1b863f4b150271c7325e7764282c3ebd08f17d82
Bertrik Sikken <bertrik@sikken.nl>
2019-06-29 10:25:29 +0200

    When running the 'geo' command, print a clickable google maps link.

--- stofananas.ino
+++ stofananas.ino
@@ -336,6 +336,7 @@ static int do_geolocate(int argc, char *argv[])
     }
 
     Serial.printf("Latitude = %f, Longitude = %f, Accuracy = %f\n", latitude, longitude, accuracy);
+    Serial.printf("http://google.com/maps/place/%f,%f\n", latitude, longitude);
     return 0;
 }
 
